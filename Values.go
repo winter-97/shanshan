33b0b66599295c94ae6e7284e52b066e477dede3
root <root@go.(none)>
2017-03-15 08:25:48 +0000

    Values test

---
+++ Values.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+    fmt.Println("go"+"lang")
+    fmt.Println("2+3=",2+3)
+    fmt.Println("7.0/3.0",7.0/3.0)
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!false)
+}
