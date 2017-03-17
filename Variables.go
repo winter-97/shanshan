e1aff805ef29ab227f9fc255f6a19f33332176d6
winter-97 <shanshan@gmail.com>
2017-03-17 00:15:08 +0000

    Variables

---
+++ Variables.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main(){
+    var a string = "initial"
+    fmt.Println("a=", a)
+
+    var b, c int = 1, 2
+    fmt.Println("b=",b)
+    fmt.Println("c=",c)
+    
+    var d = true
+    fmt.Println("d=",d)
+
+    var e int
+    fmt.Println("e=",e)
+
+    f :="short"
+    fmt.Println("f=",f)
+}
